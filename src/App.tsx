4a0d73287a0c6e7b8ad069a03dca1ff797b0ed0d
imustend <plochockitymon@gmail.com>
2022-04-11 22:31:27 +0200

    routing in github pages??

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className={classes.App}>
             <h1 className={classes.name}>BigNtertainment</h1>
-            <Link to={""}>aaaa</Link>
+            <Link to={"/"}>aaaa</Link>
         <Canvas>
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
