fdf584823e0923135e23e85094cf8492530c8900
imustend <plochockitymon@gmail.com>
2022-04-11 22:20:37 +0200

    HHAHAHHAHAHA

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import * as THREE from 'three'
 import React from 'react';
 import classes from './App.module.css';
 import Link from "./component/Link";
 import Box from "./component/3js";
+import { Canvas, useFrame } from '@react-three/fiber'
 
 function App() {
   return (
     <div className={classes.App}>
-          <h1 className={classes.name}>BigNtertainment</h1>
-          <Link to={""}>aaaa</Link>
+            <h1 className={classes.name}>BigNtertainment</h1>
+            <Link to={""}>aaaa</Link>
+        <Canvas>
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <Box position={[0, 0, 0]} />
+        </Canvas>
 
     </div>
   );
--- src/component/3js.tsx
+++ src/component/3js.tsx
@@ -1,34 +1,27 @@
 import * as THREE from 'three'
-import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 
 function Box(props: JSX.IntrinsicElements['mesh']) {
-	const ref = useRef<THREE.Mesh>(null!)
+	const mesh = useRef(null!)
 	const [hovered, hover] = useState(false)
 	const [clicked, click] = useState(false)
-	useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+	const loader = new THREE.TextureLoader();
+	// @ts-ignore
+	useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
 	return (
 		<mesh
 			{...props}
+			ref={mesh}
 			scale={clicked ? 1.5 : 1}
 			onClick={(event) => click(!clicked)}
 			onPointerOver={(event) => hover(true)}
 			onPointerOut={(event) => hover(false)}>
 			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+			{/*<sphereGeometry args={[1, 64, 32]}/>*/}
+			<meshStandardMaterial color={hovered ? '#DA1E37' : '#F8F9FA'} map={loader.load("/logo.png")} />
 		</mesh>
 	)
 }
-
-const root = document.getElementById('root')!;
-createRoot(root).render(
-	<Canvas>
-		<ambientLight />
-		<pointLight position={[10, 10, 10]} />
-		<Box position={[-1.2, 0, 0]} />
-		<Box position={[1.2, 0, 0]} />
-	</Canvas>
-)
 
 export default Box;
